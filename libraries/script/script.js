f6b1508c5988db9b2c13df3bd90ea8db4a7a9b09
ilhamnopihendri <ilhamnopihendri@gmail.com>
2021-05-07 01:58:18 +0700

    menyelesaikan login page

--- libraries/script/script.js
+++ libraries/script/script.js
@@ -1,3 +1,32 @@
+
+// ======================================Smooth scrool on click
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies ttakes to scroll to the specified areahe number of milliseconds it
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top - 70,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
+
 // =====================================================================Smooth Scroll to top
 //Get the button
 window.onscroll = function () {
@@ -11,11 +40,28 @@ function scrollFunction() {
   }
 }
 
-
 // when user click on the button, scroll to the top of the document
 function topFunction() {
   $("html, body").animate({ scrollTop: 0 }, "slow");
 }
+
+//================================== Wave \
+// ambil gambar berdasarkan id wave 1 - 4
+let wave1 = document.getElementById("wave1");
+let wave2 = document.getElementById("wave2");
+let wave3 = document.getElementById("wave3");
+let wave4 = document.getElementById("wave4");
+// ketika window di scroll jalan kan sebuah fungsi
+window.addEventListener("scroll", function () {
+  // ketika di scroll vertikal
+  let value = window.scrollY;
+  // beri style pada masing masing wave
+  wave1.style.backgroundPositionX = 400 + value * 4 + "px";
+  wave2.style.backgroundPositionX = 300 + value * -4 + "px";
+  wave3.style.backgroundPositionX = 200 + value * 2 + "px";
+  wave4.style.backgroundPositionX = 100 + value * -2 + "px";
+});
+
 // ============================================================style light slider
 $(document).ready(function () {
   $("#autoWidth").lightSlider({
@@ -56,34 +102,4 @@ btn.onclick = () => {
   return false;
 };
 
-// ======================================Smooth scrool on click
-$(document).ready(function () {
-  // Add smooth scrolling to all links
-  $("a").on("click", function (event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top,
-        },
-        800,
-        function () {
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        }
-      );
-    } // End if
-  });
-});
-
-
 // =====================================================================filter image portfolio
-
